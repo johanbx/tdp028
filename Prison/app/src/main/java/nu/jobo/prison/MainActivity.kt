0b9d69d3a1a8e1d23d25ae4073331d176c07c958
johanbx <johan.borgenstierna@outlook.com>
2018-06-11 20:13:11 +0200

    add step counter source is given in the comments

--- Prison/app/src/main/java/nu/jobo/prison/MainActivity.kt
+++ Prison/app/src/main/java/nu/jobo/prison/MainActivity.kt
@@ -1,18 +1,28 @@
 package nu.jobo.prison
 
 import android.app.Activity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import java.util.*
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), SensorEventListener {
+
+    var running = false
+    var sensorManager:SensorManager? = null
 
     lateinit var statusImage: ImageView
     lateinit var pushUpCounter: TextView
     lateinit var sitUpCounter: TextView
     lateinit var powerCounter: TextView
+    lateinit var stepCounter: TextView
 
     // Source: https://stackoverflow.com/questions/45685026/how-can-i-get-a-random-number-in-kotlin
     fun ClosedRange<Int>.random() =
@@ -22,10 +32,13 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
         statusImage = findViewById(R.id.image_prisoner_status)
         pushUpCounter = findViewById(R.id.text_total_push_ups)
         sitUpCounter = findViewById(R.id.text_total_sit_ups)
         powerCounter = findViewById(R.id.power)
+        stepCounter = findViewById(R.id.steps)
     }
 
     fun eventDied(view: View) {
@@ -82,5 +95,40 @@ class MainActivity : Activity() {
                 .inc()
                 .toString()
         powerRandomRangeIncrease(10, 20)
+    }
+
+    // Source for step counter: https://medium.com/@ssaurel/create-a-step-counter-fitness-app-for-android-with-kotlin-bbfb6ffe3ea7
+    override fun onResume() {
+        super.onResume()
+        running = true
+        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepsSensor == null) {
+            Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (running) {
+            if (event != null) {
+                stepCounter.text = stepCounter.text
+                        .toString()
+                        .toInt()
+                        .inc()
+                        .toString()
+                powerRandomRangeIncrease(100, 200)
+            }
+        }
     }
 }
