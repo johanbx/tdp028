c563b703a1ead3efdd2af7b863a0b1d039f9a536
johanbx <johan.borgenstierna@outlook.com>
2018-06-11 14:24:14 +0200

    initial commit visual mockup in place

---
+++ Prison/app/src/main/java/nu/jobo/prison/MainActivity.kt
@@ -0,0 +1,12 @@
+package nu.jobo.prison
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
