54c5df36702489da8fd0a91b96c23b3fa1e716c9
johanbx <johan.borgenstierna@outlook.com>
2018-07-05 12:32:12 +0200

    notification button implemented

--- Prison/app/src/main/java/nu/jobo/prison/MainActivity.kt
+++ Prison/app/src/main/java/nu/jobo/prison/MainActivity.kt
@@ -1,6 +1,7 @@
 package nu.jobo.prison
 
 import android.app.Activity
+import android.app.Notification
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -13,7 +14,13 @@ import java.util.*
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import android.content.Intent
+import android.support.v4.app.NotificationCompat
 import android.widget.*
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.os.Build
+import android.app.PendingIntent
+import android.support.v4.app.NotificationManagerCompat
 
 
 class MainActivity : Activity(), SensorEventListener {
@@ -21,6 +28,7 @@ class MainActivity : Activity(), SensorEventListener {
     companion object {
         const val POWER_FOR_ESCAPE = 10000
         const val RC_SIGN_IN = 123
+        const val CHANNEL_ID = "CHANNEL_ID"
     }
 
     // Authentication Providers (login ui)
@@ -33,6 +41,7 @@ class MainActivity : Activity(), SensorEventListener {
     private var running = false
     private var sensorManager:SensorManager? = null
 
+    private lateinit var escapingAttemptNotificationBuilder: NotificationCompat.Builder
     private lateinit var prisonerEvents: PrisonerEvents
     private lateinit var mAuth: FirebaseAuth
     private lateinit var stepCounter: TextView
@@ -63,16 +72,6 @@ class MainActivity : Activity(), SensorEventListener {
         setTitle(R.string.prisoner_status_captured)
     }
 
-    private fun logout() {
-        AuthUI.getInstance()
-            .signOut(this)
-            .addOnCompleteListener {
-                Toast.makeText(this, "You were logged out!", Toast.LENGTH_SHORT).show()
-                finish()
-                startActivity(intent)
-            }
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -99,6 +98,9 @@ class MainActivity : Activity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        createNotificationChannel()
+        initNotification()
+
         mAuth = FirebaseAuth.getInstance()
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -115,6 +117,48 @@ class MainActivity : Activity(), SensorEventListener {
         stepCounter = findViewById(R.id.steps)
 
         prisonerEvents = PrisonerEvents(this)
+    }
+
+    private fun initNotification(){
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        escapingAttemptNotificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("YOU ARE OUTSIDE THE PRISON AREA!")
+                .setContentText("The guards are on the way, run!!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
+    }
+
+    private fun logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    Toast.makeText(this, "You were logged out!", Toast.LENGTH_SHORT).show()
+                    finish()
+                    startActivity(intent)
+                }
     }
 
     private fun simpleEventButton(resourceId: Int, function: () -> Unit): Button {
@@ -163,6 +207,9 @@ class MainActivity : Activity(), SensorEventListener {
         val tempWonButton: Button = simpleEventButton(
                 "Win", {prisonerEvents.eventWon()})
 
+        val tempEscapeNotificationButton: Button = simpleEventButton(
+                "Notify", {escapeNotification()})
+
         return arrayOf<Button>(
                 pushUpButton,
                 sitUpButton,
@@ -173,16 +220,20 @@ class MainActivity : Activity(), SensorEventListener {
                 tempAdd1000PowerButton,
                 tempGodKillButton,
                 tryEscapeButton,
-                tempWonButton)
+                tempWonButton,
+                tempEscapeNotificationButton)
     }
 
     // Temporary
-    fun tempLogout() {
-        logout()
+    private fun escapeNotification() {
+        val notificationManager = NotificationManagerCompat.from(this)
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(123, escapingAttemptNotificationBuilder.build())
     }
     // --------------
 
-    // Source for step counter: https://medium.com/@ssaurel/create-a-step-counter-fitness-app-for-android-with-kotlin-bbfb6ffe3ea7
+    // Source for step counter:
+    // https://medium.com/@ssaurel/create-a-step-counter-fitness-app-for-android-with-kotlin-bbfb6ffe3ea7
     override fun onResume() {
         super.onResume()
         running = true
